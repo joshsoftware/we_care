29584229740d2401faba1b0a1151dde7edc8dfa1
Vishwajeetsingh Desurkar <vishwajeetsinghd@gmail.com>
2021-09-17 15:46:50 +0530

    footer  added with Josh social media links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@fortawesome/fontawesome-free/js/all";  
 require("bootstrap")
 
 // stylesheets
 require("../stylesheets/application.scss")
+
 
 Rails.start()
 Turbolinks.start()
