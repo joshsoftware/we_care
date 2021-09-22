47ef0c39be080b583b1d211dade78790bb159c0b
Vishwajeetsingh Desurkar <vishwajeetsinghd@gmail.com>
2021-09-22 23:54:20 +0530

    added logic to handle access to bid based upon auction end time

--- app/controllers/auction_items_controller.rb
+++ app/controllers/auction_items_controller.rb
@@ -2,6 +2,9 @@ class AuctionItemsController < ApplicationController
 
   def show
     @auction_item = AuctionItem.where(id: params[:id]).first
+
+    return redirect_to root_path if Time.now > @auction_item.auction.ends_at
+    
     @bid = @auction_item.bids.build
   end
 end
